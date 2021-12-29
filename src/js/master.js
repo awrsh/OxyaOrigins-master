4938b05d8704590da80273f5ce1e4dffe0dec719
abbas ebadian <abbasebadiann@gmail.com>
2021-12-30 01:43:33 +0330

    swiper lib, artistas done

--- src/js/master.js
+++ src/js/master.js
@@ -1,11 +1,39 @@
 $(function () {
     init_menu_toggler_listeners();
+    init_swiper();
 })
 
 function init_menu_toggler_listeners(){
     $(".toggler").click(toggle_mobile_menu);
 }
+function init_swiper(){
+    const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    centeredSlides: true,
+    initialSlide: 1,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints:{
+        360:{
+            slidesPerView: 2
+        }, 
+        768:{
+            slidesPerView:3
+        }
+    },
 
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+   
+});
+
+}
 function toggle_mobile_menu() {
     $("header .links").toggleClass("open");
 }
